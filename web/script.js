38f731d543c8bf95f3e285eb916b62713bf5520e
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-06 16:01:56 +0900

    拡大ボタンの追加

--- web/script.js
+++ web/script.js
@@ -4,7 +4,38 @@ function initialize() {
   var myOptions = {
     zoom: 18, /*拡大比率*/
     center: latlng, /*表示枠内の中心点*/
-    mapTypeId: google.maps.MapTypeId.ROADMAP/*表示タイプの指定*/
+    mapTypeId: google.maps.MapTypeId.ROADMAP,/*表示タイプの指定*/
+    disableDefaultUI: true, /* デフォルトのUIを無効化 */
+    zoomControl: true,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_CENTER
+    }
   };
   var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+}
+
+
+
+
+// カスタムコントロールボタン
+
+function mapPlusButton(parent) {
+  var UI = document.createElement('div');
+  UI.classList.add("map-plus");
+  parent.appendChild(UI);
+}
+
+
+
+function mapMinusButton(parent) {
+  var UI = document.createElement('div');
+  UI.classList.add("map-minus");
+  parent.appendChild(UI);
+}
+
+
+
+// ユーティリティ
+function attachStyle(map) {
+
 }
