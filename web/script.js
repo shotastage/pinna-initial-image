309296c557c016c36419a71a4a19b5e77889dac7
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-06 12:10:05 +0900

    地図を追加

--- web/script.js
+++ web/script.js
@@ -1,1 +1,10 @@
 //
+function initialize() {
+  var latlng = new google.maps.LatLng(35.388471, 139.427608);
+  var myOptions = {
+    zoom: 18, /*拡大比率*/
+    center: latlng, /*表示枠内の中心点*/
+    mapTypeId: google.maps.MapTypeId.ROADMAP/*表示タイプの指定*/
+  };
+  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+}
