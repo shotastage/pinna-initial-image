60e0ca70cf8d92529995e21c1fb6493efbe8662b
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-06 16:30:26 +0900

    ボタンのレイアウトを調整

--- web/script.js
+++ web/script.js
@@ -1,37 +1,39 @@
-//
+// Mapオブジェクト
+var map;
+// デフォルトズーム
+var zoom = 18;
+
+
+// Mapの初期化
+window.addEventListener('load', function () {
+  initialize();
+}, false);
+// ズームクリック
+document.getElementById("map_plus").addEventListener("click", function () {
+  zoom = zoom + 1;
+  map.setZoom(zoom);
+}, false);
+
+// フェードクリック
+document.getElementById("map_minus").addEventListener("click", function () {
+  zoom = zoom - 1;
+  map.setZoom(zoom);
+}, false);
+
+
+
 function initialize() {
   var latlng = new google.maps.LatLng(35.388471, 139.427608);
   var myOptions = {
-    zoom: 18, /*拡大比率*/
+    zoom: zoom, /*拡大比率*/
     center: latlng, /*表示枠内の中心点*/
     mapTypeId: google.maps.MapTypeId.ROADMAP,/*表示タイプの指定*/
-    disableDefaultUI: true, /* デフォルトのUIを無効化 */
-    zoomControl: true,
-    zoomControlOptions: {
-        position: google.maps.ControlPosition.LEFT_CENTER
-    }
+    disableDefaultUI: true /* デフォルトのUIを無効化 */
   };
-  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+  map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
 }
 
 
-
-
-// カスタムコントロールボタン
-
-function mapPlusButton(parent) {
-  var UI = document.createElement('div');
-  UI.classList.add("map-plus");
-  parent.appendChild(UI);
-}
-
-
-
-function mapMinusButton(parent) {
-  var UI = document.createElement('div');
-  UI.classList.add("map-minus");
-  parent.appendChild(UI);
-}
 
 
 
