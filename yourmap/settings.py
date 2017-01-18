6efd7531ea8a341e99075b35b591d76f01bfee9a
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-19 02:42:05 +0900

    Fix conf.

--- yourmap/settings.py
+++ yourmap/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/contents/'
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'web')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web'),
