2a6500a71c2bfedd48e1c5815a5ede3ab078e4ab
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-19 18:21:56 +0900

    Fix some issues.

--- web/script/map.js
+++ web/script/map.js
@@ -13,6 +13,8 @@ var maker = [];
 // イベントリスナ
 var listener = [];
 
+// YouTube Video ID
+ var video_id = [];
 
 
 // Gmap イニシャライザ
@@ -27,27 +29,43 @@ function init(arg) {
     map = new google.maps.Map(document.getElementById('map_canvas'), Options);
 
     for (var i = 0; i < arg.length; i++) {
-        var lat = arg[i]['lat'];
-        var lng = arg[i]['lng'];
-        var name = arg[i]['name'];
-        var PinLatLng = {
-            lat: lat,
-            lng: lng
-        };
-        maker[i] = new google.maps.Marker({
-            position: PinLatLng,
-            map: map
-        });
-        listener[i] = google.maps.event.addListener(maker[i], "click", function () {
-            if (name.match("youtu.be")) {
-                var video_id = name.split("https://youtu.be/")[1];
-            } else if (name.match("youtube.com")) {
-                var video_id = name.split("https://www.youtube.com/watch?v=")[1];
-            }
-            var html = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + video_id +'" frameborder="0" allowfullscreen></iframe>';
-            document.getElementById("insert-area").innerHTML = html;
-            document.getElementById("popup-trigger").click();
-        });
+
+        (function (n) {
+            var lat = arg[n]['lat'];
+            var lng = arg[n]['lng'];
+            var name = arg[n]['name'];
+            var PinLatLng = {
+                lat: lat,
+                lng: lng
+            };
+
+            (function(m) {
+                maker[m] = new google.maps.Marker({
+                    position: PinLatLng,
+                    map: map
+                });
+            })(n);
+
+            (function(m) {
+                listener[m] = google.maps.event.addListener(maker[m], "click", function () {
+                    if (name.match("youtu.be")) {
+                        video_id[m] = name.split("https://youtu.be/")[1];
+                    } else if (name.match("youtube.com")) {
+                        video_id[m] = name.split("https://www.youtube.com/watch?v=")[1];
+                    }
+                    var html = [];
+                    html[m] = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + video_id[m] +'" frameborder="0" allowfullscreen></iframe>';
+                    var insert_area = document.getElementById("insert-area");
+                    if (insert_area.hasChildNodes) {
+                        document.getElementById("insert-area").removeChild(insert_area.childNodes[0]);
+                    }
+                    document.getElementById("insert-area").innerHTML = html[m];
+                    document.getElementById("popup-trigger").click();
+                });
+            })(n);
+
+
+        })(i);
     }
 }
 
