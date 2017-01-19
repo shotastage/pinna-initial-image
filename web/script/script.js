41d8c23a03cc38ca778094818b477c3f23e1d792
Shota Shimazu <hornet.live.mf@gmail.com>
2017-01-20 03:33:42 +0900

    Remove comment.

--- web/script/script.js
+++ web/script/script.js
@@ -47,59 +47,6 @@ if ("geolocation" in navigator) {
 
 
 
-/*
-setInterval(function () {
-  var lat, lng;
-
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-			  // 取得したデータの整理
-			  var data = position.coords ;
-
-			  // データの整理
-			  var lat = data.latitude;
-			  var lng = data.longitude;
-
-        latlng = new google.maps.LatLng(lat, lng);
-        //initialize(pins);
-				 init(pins);
-	    },
-
-		  // [第2引数] 取得に失敗した場合の関数
-		  function(error) {
-			  var errorInfo = [
-				  "原因不明のエラーが発生しました…。" ,
-				  "位置情報の取得が許可されませんでした…。" ,
-				  "電波状況などで位置情報が取得できませんでした…。" ,
-				  "位置情報の取得に時間がかかり過ぎてタイムアウトしました…。"
-        ];
-
-			  // エラー番号
-			  var errorNo = error.code ;
-
-			  // エラーメッセージ
-			  var errorMessage = "[エラー番号: " + errorNo + "]\n" + errorInfo[ errorNo ];
-
-			  // アラート表示
-        console.log(errorMessage);
-		  },
-
-		  // [第3引数] オプション
-		  {
-			  "enableHighAccuracy": false,
-			  "timeout": 8000,
-			  "maximumAge": 2000,
-		  }
-	  );
-  } else {
-    alert("Failed to get your location.\nPlease confirm settings or GPS modules.");
-  }
-
-}, 15000);*/
-
-
-
 
 // ズームクリック
 document.getElementById("map_plus").addEventListener("click", function () {
